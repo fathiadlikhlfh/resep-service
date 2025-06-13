3ea83e723b46f1955c3b6a115cec28a6d7f81b2e
Thi <148309216+fathiadlikhlfh@users.noreply.github.com>
2025-06-13 22:12:55 +0700

    update menggunakan db neon

--- src/db.js
+++ src/db.js
@@ -1,15 +1,17 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false, // true jika mau lihat query-nya
+});
 
 module.exports = sequelize;
